c59969c36bd919eee25671096ae559bb37a910f3
ilgiz <4ilgiz@gmail.com>
2019-01-18 15:35:44 +0300

    arrays

--- src/cf/tilgiz/Main.java
+++ src/cf/tilgiz/Main.java
@@ -1,8 +1,16 @@
 package cf.tilgiz;
 
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hi");
+//        int[] arr = new int[5];
+        int[] rrr = new int[]{4,3,2};
+        int[] rew = {5,3,2,5,56,3};
+
+        System.out.println(Arrays.toString(rrr));
+        System.out.println(rew.length);
     }
+
 }
